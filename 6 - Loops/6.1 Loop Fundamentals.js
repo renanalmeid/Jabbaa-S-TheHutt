45b3454d80b1ce535f801206aee74e2d117c9dee
renanalmeid <70292070+renanalmeid@users.noreply.github.com>
2022-06-04 15:18:53 -0300

    Create 6.1 Loop Fundamentals.js

---
+++ 6 - Loops/6.1 Loop Fundamentals.js
@@ -0,0 +1,86 @@
+// ######## Foor Loop
+/*
+for (let counter = 0; counter < 4; counter++) {
+  console.log(counter);
+}
+*/
+for (let i = 5; i <11; i++){
+    console.log(i);
+  }
+
+// ######## Looping in Reverse
+/*
+// The loop below loops from 0 to 3. Edit it to loop backwards from 3 to 0
+for (let counter = 3; counter >= 0; counter--){
+  console.log(counter);
+}
+*/
+
+
+// ######## Looping through Arrays
+/*
+
+To loop through each element in an array, a for loop should use the array’s .length property in its condition.
+
+const animals = ['Grizzly Bear', 'Sloth', 'Sea Lion'];
+for (let i = 0; i < animals.length; i++){
+  console.log(animals[i]);
+}
+*/
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++ ){
+  console.log('I would love to visit ' + vacationSpots[i]);
+}
+
+// ######## Nested Loops
+/*
+
+*/
+
+
+// ########
+/*
+
+*/
+
+// ########
+/*
+
+*/
+
+// ########
+/*
+
+*/
+
+
+// ########
+/*
+
+*/
+
+
+// ########
+/*
+
+*/
+
+
+// ########
+/*
+
+*/
+
+
+// ########
+/*
+
+*/
+
+
+// ########
+/*
+
+*/
